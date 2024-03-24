1cdc55e79bf427215d560327385739885f517cb9
alibaba0010 <zakariyahali100@gmail.com>
2024-03-24 03:14:13 +0100

    commit

--- src/index.ts
+++ src/index.ts
@@ -1,117 +1,27 @@
 // cannister code goes here
-import {
-  $query,
-  $update,
-  Record,
-  StableBTreeMap,
-  Vec,
-  match,
-  Result,
-  nat64,
-  ic,
-  Opt,
-  Principal,
-  nat,
-} from "azle";
+import { StableBTreeMap, Vec, Result, Server, nat64, ic } from "azle";
 import { v4 as uuidv4 } from "uuid";
+import express from "express";
 
-type Product = Record<{
+class Product {
   id: string;
-  seller: Principal;
+  seller: string;
   name: string;
   description: string;
   image: string;
   price: nat64;
-  stock: nat;
-  createdAt: nat64;
-  updatedAt: Opt<nat64>;
-}>;
-
-type ProductPayload = Record<{
-  name: string;
-  description: string;
-  image: string;
-  price: nat64;
-  stock: nat;
-}>;
-
-const productStorage = new StableBTreeMap<string, Product>(0, 44, 1024);
-
-$query;
-export function getProducts(): Result<Vec<Product>, string> {
-  return Result.Ok(productStorage.values());
+  stock: number;
+  createdAt: Date;
+  updatedAt: Date | null;
 }
 
-$query;
-export function getProduct(id: string): Result<Product, string> {
-  return match(productStorage.get(id), {
-    Some: (product) => Result.Ok<Product, string>(product),
-    None: () =>
-      Result.Err<Product, string>(`a product with id=${id} not found`),
+const productStorage = StableBTreeMap<string, Product>(0);
+
+export default Server(() => {
+  const app = express();
+  app.use(express.json());
+  app.get("/products", (req, res) => {
+    res.json(productStorage.values());
   });
-}
-// Add product
-$update;
-export function addProduct(payload: ProductPayload): Result<Product, string> {
-  const product: Product = {
-    id: uuidv4(),
-    createdAt: ic.time(),
-    updatedAt: Opt.None,
-    seller: ic.caller(),
-    ...payload,
-  };
-  productStorage.insert(product.id, product);
-  return Result.Ok(product);
-}
-
-$update;
-export function updateProduct(
-  id: string,
-  payload: ProductPayload
-): Result<Product, string> {
-  return match(productStorage.get(id), {
-    Some: (product) => {
-      if (product.seller.toString() != ic.caller().toString()) {
-        return Result.Err<Product, string>(
-          `only a product seller can update a product`
-        );
-      }
-      const updateProduct: Product = {
-        ...product,
-        ...payload,
-        updatedAt: Opt.Some(ic.time()),
-      };
-      productStorage.insert(id, updateProduct);
-      return Result.Ok<Product, string>(updateProduct);
-    },
-    None: () => Result.Err<Product, string>(`Product with id=${id} not found`),
-  });
-}
-
-$update;
-export function deleteProduct(id: string): Result<Product, string> {
-  return match(productStorage.remove(id), {
-    Some: (deleteProduct) => {
-      if (deleteProduct.seller.toString() != ic.caller().toString()) {
-        return Result.Err<Product, string>(
-          `only a product seller can update a product`
-        );
-      }
-      return Result.Ok<Product, string>(deleteProduct);
-    },
-    None: () => Result.Err<Product, string>(`Product with id=${id} not found.`),
-  });
-}
-
-// a workaround to make uuid package work with Azle
-globalThis.crypto = {
-  getRandomValues: () => {
-    let array = new Uint8Array(32);
-
-    for (let i = 0; i < array.length; i++) {
-      array[i] = Math.floor(Math.random() * 256);
-    }
-
-    return array;
-  },
-};
+  return app.listen();
+});
